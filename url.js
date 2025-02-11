1e7b168938289bc99ee2579d69a0342c5f974770
uj810104 <uj810104@gmail.com>
2025-02-11 23:39:54 +0800

    Update url.js

--- url.js
+++ url.js
@@ -7,7 +7,12 @@
  */
 
 const urls = [
-	'https://www.surveycake.com/s/Q6DxA',
-	'https://www.surveycake.com/s/A6Q7m',
-	'https://www.surveycake.com/s/46Wl1'
+	'https://www.surveycake.com/s/3lmGr',
+	'https://www.surveycake.com/s/ZW317',
+	'https://www.surveycake.com/s/M602a',
+	'https://www.surveycake.com/s/lm3Og',
+	'https://www.surveycake.com/s/2Mx7A',
+	'https://www.surveycake.com/s/lm8Q1',
+	'https://www.surveycake.com/s/d3Nz2',
+	'https://www.surveycake.com/s/669pD',
 ];
